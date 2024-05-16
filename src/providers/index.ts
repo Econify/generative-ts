bd328055d8a4ca80c2df22e13a16f279daccd3de
jnaglick <jnaglick@gmail.com>
2024-05-16 14:18:15 -0400

    add providers module index

---
+++ src/providers/index.ts
@@ -0,0 +1,8 @@
+export { BaseModelProvider } from "./baseModelProvider";
+export * from "./openai";
+export * from "./lm_studio";
+export * from "./huggingface_inference";
+export * from "./http";
+export * from "./groq";
+export * from "./cohere";
+export * from "./aws_bedrock";

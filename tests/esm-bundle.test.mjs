dc6074fdda5729874fe62191b103caa65cdd8b08
jnaglick <jnaglick@gmail.com>
2024-05-23 16:26:36 -0400

    enhances tests/

--- tests/esm-bundle.test.mjs
+++ tests/esm-bundle.test.mjs
@@ -9,10 +9,18 @@ const modelFromMain = generativeTs.createLmStudioModelProvider({
   modelId: "lmstudio-community/Meta-Llama-3-70B-Instruct-GGUF/Meta-Llama-3-70B-Instruct-IQ1_M.gguf",
 })
 
-modelFromCore.sendRequest({ prompt: "hi" }).then((response) => {
+const sayload = {
+  prompt: "Brief History of NY Mets:",
+  system: "talk like jafar from aladdin",
+  examplePairs: [],
+  messages: [],
+  temperature: 1.0
+};
+
+modelFromCore.sendRequest(sayload).then((response) => {
   console.log(response.choices[0].message);
 });
 
-modelFromMain.sendRequest({ prompt: "hi" }).then((response) => {
+modelFromMain.sendRequest(sayload).then((response) => {
   console.log(response.choices[0].message);
 });

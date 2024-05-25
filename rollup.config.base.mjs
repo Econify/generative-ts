979236185375a9d25b1269c5bddd1087267b8383
jnaglick <jnaglick@gmail.com>
2024-05-24 20:06:44 -0400

    fix ejs breaking build

--- rollup.config.base.mjs
+++ rollup.config.base.mjs
@@ -3,12 +3,16 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import { visualizer } from 'rollup-plugin-visualizer';
+import json from '@rollup/plugin-json';
 
 export const baseConfig = {
   plugins: [
     typescript(),
     resolve(),
     commonjs(),
+    json({
+      include: '**/ejs/package.json',
+    }),
     terser(),
     visualizer({
       filename: './stats.html',

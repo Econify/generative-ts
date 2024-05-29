b9a58ce48ce5f6f7283b9f311ebbd8227c3983f5
jnaglick <jnaglick@gmail.com>
2024-05-29 16:09:54 -0400

    reorganize tests/; run integration test during ci?

--- tests/e2e/cjs-bundle.test.js
+++ tests/e2e/cjs-bundle.test.js
@@ -1,5 +1,5 @@
-const core = require('../packages/core/dist/index.cjs');
-const generativeTs = require('../packages/generative-ts/dist/index.cjs');
+const core = require('../../packages/core/dist/index.cjs');
+const generativeTs = require('../../packages/generative-ts/dist/index.cjs');
 
 const modelFromCore = core.createLmStudioModelProvider({
   modelId: "lmstudio-community/Meta-Llama-3-70B-Instruct-GGUF/Meta-Llama-3-70B-Instruct-IQ1_M.gguf",
--- tests/e2e/esm-bundle.test.mjs
+++ tests/e2e/esm-bundle.test.mjs
@@ -1,5 +1,5 @@
-import * as core from '../packages/core/dist/index.mjs';
-import * as generativeTs from '../packages/generative-ts/dist/index.mjs';
+import * as core from '../../packages/core/dist/index.mjs';
+import * as generativeTs from '../../packages/generative-ts/dist/index.mjs';
 
 const modelFromCore = core.createLmStudioModelProvider({
   modelId: "lmstudio-community/Meta-Llama-3-70B-Instruct-GGUF/Meta-Llama-3-70B-Instruct-IQ1_M.gguf",

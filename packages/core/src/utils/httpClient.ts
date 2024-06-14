d20acdfdc5ef5590ec7c1742ab020c680c692a4c
jnaglick <jnaglick@gmail.com>
2024-06-13 20:36:03 -0400

    add timeout

--- packages/core/src/utils/httpClient.ts
+++ packages/core/src/utils/httpClient.ts
@@ -1,10 +1,8 @@
-import type { HttpClient } from "@typeDefs";
+import type { HttpClient, HttpClientRequest } from "@typeDefs";
 
-export type BuiltinHttpClientOptions = Omit<
-  RequestInit,
-  "method" | "body" | "headers" // get keys of HttpClientRequest:
-> & {
+export type HttpClientOptions = Omit<RequestInit, keyof HttpClientRequest> & {
   timeout?: number;
+  // TODO retry
 };
 
 /**
@@ -12,32 +10,51 @@ export type BuiltinHttpClientOptions = Omit<
  * Built-in HttpClient implementation, wraps fetch()
  *
  */
-const httpClient: HttpClient<BuiltinHttpClientOptions> = {
-  async fetch(endpoint: string, request: BuiltinHttpClientOptions) {
+const httpClient: HttpClient<HttpClientOptions> = {
+  async fetch(endpoint: string, request: HttpClientOptions) {
+    let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
     if (!request.signal && request.timeout) {
-      // TODO
-      console.warn("Custom timeout not implemented yet!");
+      const controller = new AbortController();
+      request.signal = controller.signal;
+      timeoutId = setTimeout(() => controller.abort(), request.timeout);
     }
 
-    const response = await fetch(endpoint, request);
+    try {
+      const response = await fetch(endpoint, request);
 
-    if (!response.ok) {
-      const text = await response.text();
+      if (!response.ok) {
+        const text = await response.text();
 
-      const errorMessage = [
-        `Failed to fetch data from ${endpoint}`,
-        `status: ${response.status}`,
-        `response: ${text}`,
-      ].join("\n");
+        const errorMessage = [
+          `Failed to fetch data from ${endpoint}`,
+          `status: ${response.status}`,
+          `response: ${text}`,
+        ].join("\n");
 
-      throw new Error(errorMessage);
+        throw new Error(errorMessage);
+      }
+
+      return response.json();
+    } catch (error) {
+      if ((error as Error).name === "AbortError") {
+        throw new Error(
+          [
+            `Failed to fetch data from ${endpoint}`,
+            `Timeout (${request.timeout}ms) exceeded`,
+          ].join("\n"),
+        );
+      }
+      throw error;
+    } finally {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
     }
-
-    return response.json();
   },
 };
 
-export const getClient = (): HttpClient<BuiltinHttpClientOptions> => {
+export const getClient = (): HttpClient<HttpClientOptions> => {
   if (typeof fetch !== "function") {
     throw new Error(
       "Built-in HttpClient uses native fetch, but native fetch is not available in this environment! Please either polyfill `window.fetch`, provide a custom HttpClient implementation, or use node 18+",

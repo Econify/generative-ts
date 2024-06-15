6629458914be22cba42bd94065ea7f8228d334af
jnaglick <jnaglick@gmail.com>
2024-06-15 17:03:18 -0400

    maybe new api

--- packages/core/src/providers/index.ts
+++ packages/core/src/providers/index.ts
@@ -1,9 +1,27 @@
+// import { createAwsBedrockModelProvider } from "./aws_bedrock";
+// import { createCohereModelProvider } from "./cohere";
+// import { createGroqModelProvider } from "./groq";
+// import { createHuggingfaceInferenceModelProvider } from "./huggingface_inference";
+// import { createLmStudioModelProvider } from "./lm_studio";
+// import { createMistralModelProvider } from "./mistral";
+// import { createOpenAiChatModelProvider } from "./openai";
+
 export * from "./baseModelProvider";
-export * from "./openai";
-export * from "./lm_studio";
-export * from "./huggingface_inference";
-export * from "./http";
-export * from "./groq";
-export * from "./cohere";
 export * from "./aws_bedrock";
+export * from "./cohere";
+export * from "./groq";
+export * from "./http";
+export * from "./huggingface_inference";
+export * from "./lm_studio";
 export * from "./mistral";
+export * from "./openai";
+
+// export const ModelProviders = {
+//   bedrock: createAwsBedrockModelProvider,
+//   cohere: createCohereModelProvider,
+//   groq: createGroqModelProvider,
+//   huggingfaceInference: createHuggingfaceInferenceModelProvider,
+//   lmStudio: createLmStudioModelProvider,
+//   mistral: createMistralModelProvider,
+//   openai: createOpenAiChatModelProvider,
+// };
